95467cf9e21a5fa56a4c93e07b82d8e5a0822924
Arco Nightshade <arconightshade.raptorbytes@gmail.com>
2025-09-15 21:19:39 -0400

    Fixing bug with the swag

--- modules/commands/shell.nix
+++ modules/commands/shell.nix
@@ -1,5 +1,7 @@
 { ... }: {
   programs.bash.shellAliases = {
     trash = "gio trash"; # example: `trash not-needed/`
+    fastfetch = "fastfetch --logo nixos";
+    nixrebuild = "sudo nixos-rebuild switch --flake /home/$USER/.lyonos#workstation";
   };
 }
