7e531e0c7f908037e10c3f1b6bb72445ad0ec4eb
Arco Nightshade <arconightshade.raptorbytes@gmail.com>
2025-09-23 20:08:59 -0400

    Updating settings

--- modules/commands/nh.nix
+++ modules/commands/nh.nix
@@ -3,6 +3,6 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/user/my-nixos-config"; # sets NH_OS_FLAKE variable for you
+    flake = "/home/lyon/.lyonos"; # sets NH_OS_FLAKE variable for you
   };
 }
