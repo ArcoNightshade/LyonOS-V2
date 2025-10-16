b4b21dcfaed5542690f06906f72a72b77fff9972
Arco Nightshade <arconightshade.raptorbytes@gmail.com>
2025-10-16 11:45:30 -0400

    Removing broken thing

--- modules/apps/nushell.nix
+++ modules/apps/nushell.nix
@@ -26,7 +26,6 @@ home-manager.users.${settings.account.name} = {
     alias nixrebuild = sudo nixos-rebuild switch --flake /home/${settings.account.name}/.lyonos#workstation
     source ~/.zoxide.nu
     alias cd = z
-    $env.NH_OS_FLAKE = /home/lyon/.lyonos
   '';
   };
 }
