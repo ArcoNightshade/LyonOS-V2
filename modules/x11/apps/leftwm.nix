f9709529c813b40ea024499303b7553eaf3bcaaf
Arco Nightshade <arconightshade.raptorbytes@gmail.com>
2025-11-07 13:14:39 -0500

    Update leftwm.nix

--- modules/x11/apps/leftwm.nix
+++ modules/x11/apps/leftwm.nix
@@ -1,1 +1,15 @@
-services.xserver.windowManager.leftwm.enable = true;
+  # Enabling LeftWM
+  services.xserver.windowManager.leftwm.enable = true;
+  services.xserver.enable = true;
+  services.displayManager.ly.enable = true;
+
+  # Audio
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
