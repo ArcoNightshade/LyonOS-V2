90c3ad348aaf422dcd7c56767c70970362b44f14
Arco Nightshade <arconightshade.raptorbytes@gmail.com>
2025-11-07 13:09:53 -0500

    Update software.nix

--- modules/x11/apps/software.nix
+++ modules/x11/apps/software.nix
@@ -1,1 +1,11 @@
 
+{ pkgs, settings, ... }: {
+  home-manager.users.${settings.account.name}.home.packages = with pkgs; [
+
+    # Music/Creative
+    tidal-hifi obsidian
+
+    # Internet tools
+    firefox vesktop dino
+  ];
+}
